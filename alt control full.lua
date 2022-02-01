dccfdac264f9cc7e7a552a6f7cedd06fa7c622c3
GetGian <79874148+GetGian@users.noreply.github.com>
2022-02-01 12:52:52 +0800

    Update alt control full.lua

--- alt control full.lua
+++ alt control full.lua
@@ -1,7 +1,6 @@
 getgenv().Hostt = "Gian" -- Put The Person That You Want To Have Control(Do Not Execute This On The Accout That's Gonna Get Money)
 getgenv().prefix = "!" -- Self Explanatory
 getgenv().chat = "Gian is pro" -- Example when i do !chat it will say whatever is in the Quotation mark
-getgenv().count = "" -- It Will Get A MoneyCounter(Only Counts 7k)
 --[[ Commands  ]]-- 
 -- drop - drops money
 -- stop - stops dropping money
@@ -14,6 +13,7 @@ getgenv().count = "" -- It Will Get A MoneyCounter(Only Counts 7k)
 -- reset - force reset
 -- chat - chats what ever is in getgenv().chat == "im a pro"
 -- wallet - equips wallet
+-- setup - do !bring first then do !setup it will go into godmode and freeze
 
 
 -- anti afk
